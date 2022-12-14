b5cc5d3cab1bd0254fc477324fb266dc75c25bce
Gyozadaigaku <gaspardcube15@gmail.com>
2022-12-14 16:34:18 +0900

    feat: add random pk id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,19 @@
 import type { NextPage } from "next";
+import React, { useState, useEffect } from "react";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.hello.useQuery({ text: "client" });
-  if (!hello.data) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div>
-      <p>{hello.data.greeting}</p>
-    </div>
-  );
+  const [first, second] = getOptionsForVote();
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokémon is Rounder?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex items-center justify-between max-w-2xl ">
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{first}</div>
         <div className="p-8">VS</div>
-        <div className="w-16 h-16 bg-red-200"></div>
+        <div className="w-16 h-16 bg-red-800">{second}</div>
       </div>
     </div>
   );
